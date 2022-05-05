a0f1fb62c5369ce78bb538814b01e5761bc99b63
TulioCP <tuliocp1999@gmail.com>
2022-05-04 21:57:27 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,13 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New ID = "+newSeller.getId());
+
+        System.out.println("=== Teste 5: Seller Update ===");
+        seller = sellerDao.findeByID(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completed!");
+
     }
 
 }
